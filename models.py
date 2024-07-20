9bf394fb552096dec5b86eaa92c39fedbdf134d0
Amir <167396000+byt3spy@users.noreply.github.com>
2024-07-20 10:57:11 +0330

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from app import db
+
+# User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+# Item model
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200))
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
